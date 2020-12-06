d11fed73bd66396e22846b79193b574cd934c786
Pouriya <pouriya.un@gmail.com>
2020-12-06 13:01:33 +0330

    End of section 7

--- client-app-ts/src/index.tsx
+++ client-app-ts/src/index.tsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import './app/layout/styles.css';
 import 'semantic-ui-css/semantic.min.css';
 import App from './app/layout/App';
-import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <>
